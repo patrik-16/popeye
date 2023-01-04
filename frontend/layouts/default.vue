26d3f3358facdec0fd7df31062c6dfc792044f48
christinamoser <christina.moser1@stud.fh-campuswien.ac.at>
2023-01-04 15:29:55 +0100

    new landing page start with form

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,9 +2,8 @@
   <v-app dark>
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
       :clipped="clipped"
-      fixed
+      temporary
       app
     >
       <v-list>
@@ -30,31 +29,11 @@
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+      <v-spacer />
       <v-toolbar-title>{{ title }}</v-toolbar-title>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
+      <v-btn>
+        Login
       </v-btn>
     </v-app-bar>
     <v-main>
@@ -62,23 +41,6 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
       app
@@ -95,23 +57,30 @@ export default {
     return {
       clipped: false,
       drawer: false,
-      fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-home-outline',
+          title: 'Home',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: 'mdi-arrow-right',
+          title: 'Start your Journey',
+          to: '/formPage'
+        },
+        {
+          icon: 'mdi-account-outline',
+          title: 'Login',
+          to: '/login'
+        },
+        /* TODO: only show if login is true ..... */
+        {
+          icon: 'mdi-account',
+          title: 'Profile',
+          to: '/profile'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Popeye'
     }
   }
 }
---
+++ frontend/pages/profile.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Profile Page</h1>
+</template>
+
+<script>
+export default {
+  name: 'profile'
+}
+</script>
+
+<style scoped>
+
+</style>
