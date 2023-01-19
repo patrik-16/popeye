a95f6c109772f0ad657758d206e57c1c6c71a354
Patrik <91659503+patrik-16@users.noreply.github.com>
2023-01-19 17:09:03 +0100

    Still working

--- backend/src/main/java/com/popeye/backend/services/ProgramService.java
+++ backend/src/main/java/com/popeye/backend/services/ProgramService.java
@@ -4,7 +4,11 @@ import com.popeye.backend.entity.*;
 import com.popeye.backend.enums.*;
 import com.popeye.backend.repos.ExerciseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.StreamingHttpOutputMessage;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -12,15 +16,6 @@ import java.util.List;
 public class ProgramService {
     @Autowired
     ExerciseRepository exerciseRepository;
-    /*will be important for ordering by goal
-        for (int i = 0; i < beginnerExercises.size(); i++) {
-            if (beginnerExercises.get(i).getBodypart().containsAll(userinput.getPriorities())) {
-
-                Exercise foundEx = beginnerExercises.get(i);
-                beginnerExercises.remove(beginnerExercises.get(i));
-                beginnerExercises.add(0, foundEx);
-            }
-        }*/
 
     public Program createProgram(Userinput userinput) {
         if (userinput.getExperience().equals(Experience.BEGINNER)) {
@@ -50,6 +45,57 @@ public class ProgramService {
      */
     private Program generateAdvancedProgram(Userinput userinput) {
 
+        HashMap<String, Integer> fullbody = new HashMap<String, Integer>();
+        fullbody.put("CHEST", 70);
+        fullbody.put("LATS", 70);
+        fullbody.put("TRAPS", 70);
+        fullbody.put("QUADS", 70);
+        fullbody.put("GLUTES", 70);
+        fullbody.put("HAMS", 70);
+        fullbody.put("CALVES", 70);
+        fullbody.put("ABS", 70);
+        HashMap<String, Integer> upper = new HashMap<String, Integer>();
+        fullbody.put("QUADS", 70);
+        fullbody.put("TRICEP", 70);
+        fullbody.put("CHEST", 70);
+        HashMap<String, Integer> lower = new HashMap<String, Integer>();
+        fullbody.put("QUADS", 70);
+        fullbody.put("TRICEP", 70);
+        fullbody.put("CHEST", 70);
+        HashMap<String, Integer> upper1 = new HashMap<String, Integer>();
+        fullbody.put("QUADS", 70);
+        fullbody.put("TRICEP", 70);
+        fullbody.put("CHEST", 70);
+        HashMap<String, Integer> lower1 = new HashMap<String, Integer>();
+        fullbody.put("QUADS", 70);
+        fullbody.put("TRICEP", 70);
+        fullbody.put("CHEST", 70);
+        HashMap<String, Integer> upper2 = new HashMap<String, Integer>();
+        fullbody.put("QUADS", 70);
+        fullbody.put("TRICEP", 70);
+        fullbody.put("CHEST", 70);
+        HashMap<String, Integer> lower2 = new HashMap<String, Integer>();
+        fullbody.put("QUADS", 70);
+        fullbody.put("TRICEP", 70);
+        fullbody.put("CHEST", 70);
+        HashMap<String, Integer> push1 = new HashMap<String, Integer>();
+        fullbody.put("QUADS", 70);
+        fullbody.put("TRICEP", 70);
+        fullbody.put("CHEST", 70);
+        HashMap<String, Integer> pull1 = new HashMap<String, Integer>();
+        fullbody.put("QUADS", 70);
+        fullbody.put("TRICEP", 70);
+        fullbody.put("CHEST", 70);
+        HashMap<String, Integer> push2 = new HashMap<String, Integer>();
+        fullbody.put("QUADS", 70);
+        fullbody.put("TRICEP", 70);
+        fullbody.put("CHEST", 70);
+        HashMap<String, Integer> pull2 = new HashMap<String, Integer>();
+        fullbody.put("QUADS", 70);
+        fullbody.put("TRICEP", 70);
+        fullbody.put("CHEST", 70);
+
+
         switch (userinput.getDaysPerWeek()) {
             case 1 -> {
                 return null; //TODO handle error exception in frontend
@@ -58,18 +104,23 @@ public class ProgramService {
                 ProgramSession sessionOne = new ProgramSession(exerciseRepository.getAllAdvancedExercisesByDifficultyAndGoal(Difficulty.HARD, userinput.getGoal()), 1);
                 ProgramSession sessionTwo = new ProgramSession(exerciseRepository.getAllAdvancedExercisesByDifficultyAndGoal(Difficulty.MEDIUM, userinput.getGoal()), 2);
 
-                Program program = new Program(List.of(sessionOne, sessionTwo));
+                ProgramSession filteredSessionOne = sessionAdaptation(userinput, sessionOne, fullbody);
+                ProgramSession filteredSessionTwo = sessionAdaptation(userinput, sessionTwo, fullbody);
+                Program program = new Program(List.of(filteredSessionOne, filteredSessionTwo));
 
-                return sessionAdaption(userinput, program);
+                return programAdaptation(userinput, program);
             }
             case 3 -> {
                 ProgramSession sessionOne = new ProgramSession(exerciseRepository.getAllAdvancedExercisesByDifficultyAndGoal(Difficulty.HARD, userinput.getGoal()), 1);
                 ProgramSession sessionTwo = new ProgramSession(exerciseRepository.getAllAdvancedExercisesByDifficultyAndGoal(Difficulty.MEDIUM, userinput.getGoal()), 2);
                 ProgramSession sessionThree = new ProgramSession(exerciseRepository.getAllAdvancedExercisesByDifficultyAndGoal(Difficulty.HARD, userinput.getGoal()), 3);
 
-                Program program =  new Program(List.of(sessionOne, sessionTwo, sessionThree));
+                ProgramSession filteredSessionOne = sessionAdaptation(userinput, sessionOne, upper);
+                ProgramSession filteredSessionTwo = sessionAdaptation(userinput, sessionTwo, lower);
+                ProgramSession filteredSessionThree = sessionAdaptation(userinput, sessionThree, fullbody);
+                Program program =  new Program(List.of(filteredSessionOne, filteredSessionTwo, filteredSessionThree));
 
-                return sessionAdaption(userinput, program);
+                return programAdaptation(userinput, program);
             }
             case 4 -> {
                 ProgramSession sessionOne = new ProgramSession(exerciseRepository.getAllAdvancedExercisesByDifficultyAndGoal(Difficulty.HARD, userinput.getGoal()), 1);
@@ -77,8 +128,13 @@ public class ProgramService {
                 ProgramSession sessionThree = new ProgramSession(exerciseRepository.getAllAdvancedExercisesByDifficultyAndGoal(Difficulty.HARD, userinput.getGoal()), 3);
                 ProgramSession sessionFour = new ProgramSession(exerciseRepository.getAllAdvancedExercisesByDifficultyAndGoal(Difficulty.MEDIUM, userinput.getGoal()), 4);
 
-                Program program = new Program(List.of(sessionOne, sessionTwo, sessionThree, sessionFour));
-                return sessionAdaption(userinput, program);
+                ProgramSession filteredSessionOne = sessionAdaptation(userinput, sessionOne, upper1);
+                ProgramSession filteredSessionTwo = sessionAdaptation(userinput, sessionTwo, lower1);
+                ProgramSession filteredSessionThree = sessionAdaptation(userinput, sessionThree, upper2);
+                ProgramSession filteredSessionFour = sessionAdaptation(userinput, sessionFour, lower2);
+                Program program =  new Program(List.of(filteredSessionOne, filteredSessionTwo, filteredSessionThree, filteredSessionFour));
+
+                return programAdaptation(userinput, program);
             }
             case 5 -> {
                 ProgramSession sessionOne = new ProgramSession(exerciseRepository.getAllAdvancedExercisesByDifficultyAndGoal(Difficulty.HARD, userinput.getGoal()), 1);
@@ -87,13 +143,43 @@ public class ProgramService {
                 ProgramSession sessionFour = new ProgramSession(exerciseRepository.getAllAdvancedExercisesByDifficultyAndGoal(Difficulty.MEDIUM, userinput.getGoal()), 4);
                 ProgramSession sessionFive = new ProgramSession(exerciseRepository.getAllAdvancedExercisesByDifficultyAndGoal(Difficulty.HARD, userinput.getGoal()), 5);
 
-                Program program = new Program(List.of(sessionOne, sessionTwo, sessionThree, sessionFour, sessionFive));
+                ProgramSession filteredSessionOne = sessionAdaptation(userinput, sessionOne, push1);
+                ProgramSession filteredSessionTwo = sessionAdaptation(userinput, sessionTwo, push2);
+                ProgramSession filteredSessionThree = sessionAdaptation(userinput, sessionThree, push1);
+                ProgramSession filteredSessionFour = sessionAdaptation(userinput, sessionFour, push2);
+                ProgramSession filteredSessionFive = sessionAdaptation(userinput, sessionFive, pull2);
 
-                return sessionAdaption(userinput, program);
+                Program program =  new Program(List.of(filteredSessionOne, filteredSessionTwo, filteredSessionThree, filteredSessionFour, filteredSessionFive));
+
+                return programAdaptation(userinput, program);
             }
         }
         return null; //TODO handle error exception in frontend
     }
+
+    private ProgramSession sessionAdaptation(Userinput userinput, ProgramSession programSession, HashMap<String, Integer> filteringMap) {
+
+        List<Exercise> filteredExercises = new ArrayList<>();
+
+        filteringMap.forEach(
+            (key, value)
+                ->  {
+                    for (int i = 0; i < programSession.getExerciseList().size(); i++) {
+                        if (programSession.getExerciseList().get(i).getBodypartToEffectiveness().containsKey(key)) {
+                            String effectivness = programSession.getExerciseList().get(i).getBodypartToEffectiveness().get(key);
+                            if (Integer.valueOf(effectivness) >= Integer.valueOf(value)) {
+                                filteredExercises.add(programSession.getExerciseList().get(i));
+
+                            }
+                        }
+                      }
+                    System.out.println(key + " " + value);
+                }
+        );
+
+        return new ProgramSession(filteredExercises, programSession.getDay());
+    }
+
 
     /***
      * This function does the actual fitting of one programSession -> adaptes time and rest ?? or reduces the amount
@@ -102,9 +188,10 @@ public class ProgramService {
      * @param currentProgram
      * @return the adapted Program
      */
-    private Program sessionAdaption(Userinput userinput, Program currentProgram) {
+    private Program programAdaptation(Userinput userinput, Program currentProgram) {
 
         List<Exercise> remainingExercises;
+        //String priority = userinput.
 
         //one ProgramSession per day
         for (ProgramSession currentSession : currentProgram.getProgram()) {
