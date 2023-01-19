3e049ffa66d37332556f618e10ef94a84c7b6e6a
christinamoser <christina.moser1@stud.fh-campuswien.ac.at>
2023-01-19 11:02:26 +0100

    pdf in webstorm

--- frontend/pages/program.vue
+++ frontend/pages/program.vue
@@ -1,10 +1,20 @@
 <template>
-  <h1>Program Page</h1>
+  <v-app>
+    <div>
+      <h1>Page for the last question</h1>
+      <v-btn outlined @click="downloadPDF()">download pdf</v-btn>
+    </div>
+  </v-app>
 </template>
 
 <script>
+
 export default {
   name: 'Program',
+
+  data: () => ({
+    pdf: ''
+  }),
 
   methods: {
     /**
@@ -12,6 +22,25 @@ export default {
      */
     toForm () {
       this.$router.push('/program')
+    },
+    async downloadPDF () {
+      try {
+        const response = await fetch('http://localhost:8080/pdf/generate')
+        const blob = await response.blob()
+        const newBlob = new Blob([blob])
+        const blobUrl = window.URL.createObjectURL(newBlob)
+        const link = document.createElement('a')
+        link.href = blobUrl
+        link.setAttribute('download', 'Program.pdf')
+        document.body.appendChild(link)
+        link.click()
+        link.parentNode.removeChild(link)
+
+        // clean up Url
+        window.URL.revokeObjectURL(blobUrl)
+      } catch (e) {
+        console.log(e)
+      }
     }
   }
 }
