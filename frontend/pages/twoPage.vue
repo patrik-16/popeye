e8bff9fc416e58b251e58e9e879f4ce6d4ac6031
christinamoser <christina.moser1@stud.fh-campuswien.ac.at>
2023-01-21 21:37:32 +0100

    page two done :)

--- frontend/pages/twoPage.vue
+++ frontend/pages/twoPage.vue
@@ -1,79 +1,83 @@
 <template>
-  <v-container class="pa-4 text-center">
-    <v-row>
-      <v-card-text
-        class="text-h4"
-      >
-        <h2>How much experience do you have in the gym?</h2>
-        <div><br></div>
-      </v-card-text>
-    </v-row>
-    <v-row
-      class="fill-height"
-      align="center"
-      justify="center"
-    >
-      <template
-        v-for="(item, i) in items"
-      >
-        <v-col
-          :key="i"
-          cols="12"
-          md="4"
+  <v-div class="pa-lg-15">
+    <v-container class="pa-lg-15 text-center">
+      <v-row>
+        <v-card-text
+          class="text-sm-subtitle-1"
         >
-          <v-hover v-slot="{ hover }">
-            <v-card
-              :elevation="hover ? 12 : 2"
-              :class="{ 'on-hover': hover }"
-              @click="submit(i)"
-            >
-              <v-img
-                :src="item.img"
-                height="225px"
+          <h2>How much experience do you have in the gym?</h2>
+          <div><br></div>
+        </v-card-text>
+      </v-row>
+      <v-row
+        class="fill-height"
+        align="center"
+        justify="center"
+      >
+        <template
+          v-for="(item, i) in items"
+        >
+          <v-col
+            :key="i"
+            cols="12"
+            md="4"
+          >
+            <v-hover v-slot="{ hover }">
+              <v-card
+                :elevation="hover ? 12 : 2"
+                :class="{ 'on-hover': hover }"
+                @click="submit(i)"
               >
-                <v-card-title class="text-h4 white--text">
-                  <v-row
-                    class="fill-height flex-column"
-                    justify="space-between"
-                  >
-                    <p class="mt-10 subheading">
-                      {{ item.title }}
-                    </p>
-
-                    <div>
-                      <p class="ma-0 text-body-1 font-weight-bold">
-                        {{ item.text }}
-                      </p>
-                      <p class="text-caption font-weight-bold">
-                        {{ item.subtext }}
-                      </p>
-                    </div>
-
-                    <div class="align-self-center">
-                      <v-btn
-                        v-for="(icon, index) in icons"
-                        :key="index"
-                        :class="{ 'show-btns': hover }"
-                        :color="transparent"
-                        icon
+                <v-img
+                  :src="item.img"
+                  height="15rem"
+                >
+                  <div class="pillar" >
+                    <v-card-title class="text-h4 white--text">
+                      <v-row
+                        class="fill-height flex-column"
+                        justify="space-between"
                       >
-                        <v-icon
-                          :class="{ 'show-btns': hover }"
-                          :color="transparent"
-                        >
-                          {{ icon }}
-                        </v-icon>
-                      </v-btn>
-                    </div>
-                  </v-row>
-                </v-card-title>
-              </v-img>
-            </v-card>
-          </v-hover>
-        </v-col>
-      </template>
-    </v-row>
-  </v-container>
+                        <p class="mt-14 subheading">
+                          {{ item.title }}
+                        </p>
+
+                        <div>
+                          <p class="ma-0 text-body-1 font-weight-bold">
+                            {{ item.text }}
+                          </p>
+                          <p class="text-caption font-weight-bold">
+                            {{ item.subtext }}
+                          </p>
+                        </div>
+
+                        <div class="align-self-center">
+                          <v-btn
+                            v-for="(icon, index) in icons"
+                            :key="index"
+                            :class="{ 'show-btns': hover }"
+                            :color="transparent"
+                            icon
+                          >
+                            <v-icon
+                              :class="{ 'show-btns': hover }"
+                              :color="transparent"
+                            >
+                              {{ icon }}
+                            </v-icon>
+                          </v-btn>
+                        </div>
+                      </v-row>
+                    </v-card-title>
+                  </div>
+                </v-img>
+              </v-card>
+            </v-hover>
+          </v-col>
+        </template>
+      </v-row>
+    </v-container>
+  </v-div>
 </template>
 
 <script>
@@ -134,6 +138,11 @@ export default {
 
 .show-btns {
   color: rgba(255, 255, 255, 1) !important;
+}
+
+.pillar {
+  background: rgba(0, 0, 0, 0.6);
+  height: 15rem;
 }
 
 </style>
