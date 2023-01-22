a7dfe380d087fdabd74ce3275ea2fed0fed93319
Slavica <54640856+Slavica-lab@users.noreply.github.com>
2023-01-22 14:28:28 +0100

    Beginner changed

--- frontend/pages/program.vue
+++ frontend/pages/program.vue
@@ -96,8 +96,6 @@ export default {
     this.$data.formDataObject.age = localStorage.getItem('age')
     this.$data.formDataObject.experience = localStorage.getItem('experience')
     if (this.$data.formDataObject.experience === 'BEGINNER') {
-      this.$data.formDataObject.goal = ''
-      this.$data.formDataObject.priorities = ''
       this.$data.formDataObject.daysPerWeek = localStorage.getItem('daysPerWeek')
       this.$data.formDataObject.timePerDay = localStorage.getItem('timePerDay')
     } else if (this.$data.formDataObject.experience === 'ADVANCED') {
